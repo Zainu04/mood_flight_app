9f6fe53bbe856c4365c8745f93fb8fbea0c81db8
Zainu04 <akhtarzainab2010@gmail.com>
2025-11-06 00:10:29 -0500

    Update header and mobile navigation with hamburger menu

---
+++ static/script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('.nav_menu');
+
+burger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
